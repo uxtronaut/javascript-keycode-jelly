4947a93d5fcf903ad5bf265fcb37d537be0fc851
Andrew Butterfield <andrewcbutterfield@gmail.com>
2012-12-08 07:33:24 -0700

    First commit with source code files. Key code constants have been implemented.

---
+++ key.js
@@ -0,0 +1,208 @@
+(function() {
+  var Key;
+
+  Key = (function() {
+
+    function Key() {}
+
+    Key.BACKSPACE = 8;
+
+    Key.TAB = 9;
+
+    Key.ENTER = 13;
+
+    Key.SHIFT = 16;
+
+    Key.CTRL = 17;
+
+    Key.ALT = 18;
+
+    Key.PAUSE_BREAK = 19;
+
+    Key.CAPS_LOCK = 20;
+
+    Key.ESCAPE = 27;
+
+    Key.PAGE_UP = 33;
+
+    Key.PAGE_DOWN = 34;
+
+    Key.END = 35;
+
+    Key.HOME = 36;
+
+    Key.LEFT_ARROW = 37;
+
+    Key.UP_ARROW = 38;
+
+    Key.RIGHT_ARROW = 39;
+
+    Key.DOWN_ARROW = 40;
+
+    Key.INSERT = 45;
+
+    Key.DELETE = 46;
+
+    Key._0 = 48;
+
+    Key._1 = 49;
+
+    Key._2 = 50;
+
+    Key._3 = 51;
+
+    Key._4 = 52;
+
+    Key._5 = 53;
+
+    Key._6 = 54;
+
+    Key._7 = 55;
+
+    Key._8 = 56;
+
+    Key._9 = 57;
+
+    Key.A = 65;
+
+    Key.B = 66;
+
+    Key.C = 67;
+
+    Key.D = 68;
+
+    Key.E = 69;
+
+    Key.F = 70;
+
+    Key.G = 71;
+
+    Key.H = 72;
+
+    Key.I = 73;
+
+    Key.J = 74;
+
+    Key.K = 75;
+
+    Key.L = 76;
+
+    Key.M = 77;
+
+    Key.N = 78;
+
+    Key.O = 79;
+
+    Key.P = 80;
+
+    Key.Q = 81;
+
+    Key.R = 82;
+
+    Key.S = 83;
+
+    Key.T = 84;
+
+    Key.U = 85;
+
+    Key.V = 86;
+
+    Key.W = 87;
+
+    Key.X = 88;
+
+    Key.Y = 89;
+
+    Key.Z = 90;
+
+    Key.LEFT_WINDOW_KEY = 91;
+
+    Key.RIGHT_WINDOW_KEY = 92;
+
+    Key.SELECT_KEY = 93;
+
+    Key.NUM_PAD_0 = 96;
+
+    Key.NUM_PAD_1 = 97;
+
+    Key.NUM_PAD_2 = 98;
+
+    Key.NUM_PAD_3 = 99;
+
+    Key.NUM_PAD_4 = 100;
+
+    Key.NUM_PAD_5 = 101;
+
+    Key.NUM_PAD_6 = 102;
+
+    Key.NUM_PAD_7 = 103;
+
+    Key.NUM_PAD_8 = 104;
+
+    Key.NUM_PAD_9 = 105;
+
+    Key.NUM_PAD_ASTERISK = 106;
+
+    Key.NUM_PAD_PLUS = 107;
+
+    Key.NUM_PAD_MINUS = 109;
+
+    Key.NUM_PAD_FOWARD_SLASH = 111;
+
+    Key.F1 = 112;
+
+    Key.F2 = 113;
+
+    Key.F3 = 114;
+
+    Key.F4 = 115;
+
+    Key.F5 = 116;
+
+    Key.F6 = 117;
+
+    Key.F7 = 118;
+
+    Key.F8 = 119;
+
+    Key.F9 = 120;
+
+    Key.F10 = 121;
+
+    Key.F11 = 122;
+
+    Key.F12 = 123;
+
+    Key.NUM_LOCK = 144;
+
+    Key.SCROLL_LOCK = 145;
+
+    Key.SEMI_COLON = 186;
+
+    Key.EQUALS = 187;
+
+    Key.COMMA = 188;
+
+    Key.DASH = 189;
+
+    Key.PERIOD = 190;
+
+    Key.FORWARD_SLASH = 191;
+
+    Key.GRAVE = 192;
+
+    Key.LEFT_BRACKET = 219;
+
+    Key.BACK_SLASH = 220;
+
+    Key.RIGHT_BRACKET = 221;
+
+    Key.SINGLE_QUOTE = 222;
+
+    return Key;
+
+  })();
+
+  this.Key = Key;
+
+}).call(this);
